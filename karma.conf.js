38f0c007d9bd1370138512b81056ae9e80174459
Nicolas Benjamin Cruz Carpio <nicolasbncruz@hotmail.com>
2022-08-14 16:40:06 -0500

    add components

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/examen-final'),
+      dir: require('path').join(__dirname, './coverage/sunat'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'examen-final'`, () => {
+  it(`should have as title 'sunat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('examen-final');
+    expect(app.title).toEqual('sunat');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('examen-final app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sunat app is running!');
   });
 });
